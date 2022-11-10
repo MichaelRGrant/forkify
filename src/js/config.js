8144e48c0a75c3677fd5fe862edb2ea63e26d3f2
Michael Grant <mgrant@Michaels-MacBook-Pro.local>
2022-11-10 12:45:10 -0800

    why not

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes';
+export const TIMEOUT = 10;
+export const RESULTS_PER_PAGE = 10;
+export const KEY = '43970932-da7f-45ad-8a46-2e35de99ea5e';
+export const MODAL_CLOSE_SECONDS = 1.75;
