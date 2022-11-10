4037c8fa70bfe9eaf34d6fcf65d8ce9ec4982aa1
Michael Grant <mgrant@Michaels-MacBook-Pro.local>
2022-11-10 13:03:35 -0800

    first real commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '43970932-da7f-45ad-8a46-2e35de99ea5e';
+export const MODAL_CLOSE_SEC = 2.5;
